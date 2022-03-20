65a8e8dd222c8b130b5f203db1eb1af6a250bd8f
Alice (FedulovPF) <goparegiri@gmail.com>
2022-03-21 01:42:13 +0300

    code: добавлен вывод делениия

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int main()
 {
     int a, b;
     cout << "Enter A and B: "; cin >> a >> b;
-    cout << "A + B = " << a + b << endl << "A - B = " << a - b << endl << "A * B = " << a * b;
+    cout << "A + B = " << a + b << endl << "A - B = " << a - b << endl << "A * B = " << a * b << endl << "A / B = " << a / b;
     return 0;
 }
